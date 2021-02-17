d5c912a1e30336249f6b96c44d868f3db57572cf
Luis Felipe <godoiluisfelipe@gmail.com>
2021-02-17 02:15:22 -0300

    feat: added paginate function as well

--- pagination/pagination.go
+++ pagination/pagination.go
@@ -5,15 +5,37 @@ import (
 	"sort"
 )
 
+func pagination(itemsPerPage, pageNumber int32, items []string) {
+	totalPages := len(items) / int(itemsPerPage)
+	restDivision := len(items) % int(itemsPerPage)
+	result := make([][]string, totalPages)
+
+	if restDivision == 0 {
+		for i := 0; i < totalPages; i++ {
+			result[i] = append(result[i], items[i])
+		}
+	}
+}
+
+func sortItems(sortOrder int32, items []string) []string {
+
+	if sortOrder == 0 {
+		sort.Sort(sort.StringSlice(items))
+	} else {
+		sort.Sort(sort.Reverse(sort.StringSlice(items)))
+	}
+
+	return items
+}
+
 func fetchItemsToDisplay(items [][]string, sortParameter, sortOrder, itemsPerPage, pageNumber int32) [][]string {
 
 	var n, nNames int = len(items), 0
-	//var finalItensFetched [][]string
-	var arrName /*, arrRelevance, arrPrice*/ []string
+	var arrItems []string
 
 	for i := 0; i < n; i++ {
 		for _, s := range items {
-			arrName = append(arrName, s[i])
+			arrItems = append(arrItems, s[i])
 			nNames++
 			if nNames == n {
 				break
@@ -24,14 +46,9 @@ func fetchItemsToDisplay(items [][]string, sortParameter, sortOrder, itemsPerPag
 		}
 	}
 
-	if sortOrder == 0 {
-		sort.Sort(sort.StringSlice(arrName))
-	} else {
-		sort.Sort(sort.Reverse(sort.StringSlice(arrName)))
-	}
+	arrItems = sortItems(sortOrder, arrItems)
 
-	fmt.Println(n)
-	fmt.Println(arrName)
+	pagination(itemsPerPage, pageNumber, arrItems)
 
 	return items
 }
@@ -43,6 +60,9 @@ func main() {
 		{"jose", "10", "15"},
 		{"adalto", "3", "4"},
 		{"zoraide", "17", "18"},
+		{"dirce", "10", "15"},
+		{"ana", "3", "4"},
+		{"rosilda", "17", "18"},
 	}
 
 	var sortParameter, sortOrder, itemsPerPage, pageNumber int32 = 1, 0, 2, 1
